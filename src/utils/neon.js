d47759dff76c03fbb8bc0882d5f5fd2024c89b42
git-alice <art.oxbow@gmail.com>
2020-10-20 23:37:18 +0300

    Fix neon bug

--- src/utils/neon.js
+++ src/utils/neon.js
@@ -1,66 +1,33 @@
-// let document = window.document;
-// console.log('document', document.getElementsByClassName('neon_on_off'))
-// let textHolder = document.getElementsByClassName('neon_on_off')[0],
-//     text = textHolder.innerHTML,
-//     chars = text.length,
-//     newText = '',
-//     i;
-//
-// for (i = 0; i < chars; i += 1) {
-//     newText += '<i>' + text.charAt(i) + '</i>';
-// }
-//
-// textHolder.innerHTML = newText;
-//
-// let letters = document.getElementsByTagName('i'),
-//     flickers = [5, 7, 9, 11];
-//
-//
-// let randomLetter,
-//     flickerNumber,
-//     counter;
-//
-// export function randomFromInterval(from,to) {
-//     return Math.floor(Math.random()*(to-from+1)+from);
-// }
-//
-// export function hasClass(element, cls) {
-//     return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
-// }
-//
-// export function flicker() {
-//     counter += 1;
-//
-//     if (counter === flickerNumber) {
-//         return;
-//     }
-//
-//     setTimeout(function () {
-//         if(hasClass(randomLetter, 'off')) {
-//             randomLetter.className = '';
-//         }
-//         else {
-//             randomLetter.className = 'off';
-//         }
-//
-//         flicker();
-//     }, 30);
-// }
-//
-//
-// export default function neon_loop() {
-//     let rand = randomFromInterval(500,2000);
-//     let randomLetter, flickerNumber, counter;
-//
-//     randomLetter = randomFromInterval(0, 5);
-//     randomLetter = letters[randomLetter];
-//
-//     flickerNumber = randomFromInterval(0, 3);
-//     flickerNumber = flickers[flickerNumber];
-//
-//     setTimeout(function() {
-//         counter = 0;
-//         flicker();
-//         neon_loop();
-//     }, rand);
-// }
+function randomFromInterval(from,to) {
+    return Math.floor(Math.random()*(to - from) + from);
+}
+
+
+function hasClass(element, cls) {
+    return element.classList.contains(cls)
+}
+
+function flicker(randomLetter, flickerNumber) {
+    if (flickerNumber === 0) return;
+
+    setTimeout(function () {
+        if (hasClass(randomLetter, 'off')) randomLetter.className = '';
+        else randomLetter.className = 'off';
+
+        flicker(randomLetter, flickerNumber - 1);
+    }, 30);
+}
+
+export default function loop(letters) {
+    let flickers = [5, 8, 9, 12];
+
+    let randInterval = randomFromInterval(1000, 5000);
+
+    let randomLetter = letters[randomFromInterval(0, letters.length)];
+    let flickerNumber = flickers[randomFromInterval(0, flickers.length)];
+
+    setTimeout(function () {
+        flicker(randomLetter, flickerNumber);
+        loop(letters);
+    }, randInterval)
+}
--- src/views/land/Land.vue
+++ src/views/land/Land.vue
@@ -1,17 +1,18 @@
 <template lang="pug">
   .neon_wrapper
-    .text__neon YOU ARE
-      | <br> WE<span class="neon_on_off">LCO</span>ME
+    .text__neon
+      | YO<i>U</i> ARE
+      | <br> WE<i>L</i><i>C</i><i>O</i></span>ME
 </template>
 
 <script>
-import neon_loop from '../../utils/neon'
+import neonLoop from '../../utils/neon'
 
 export default {
   name: "Land",
   mounted() {
-    // neon_loop()
-    console.log(document.getElementsByClassName('neon_on_off')[0])
+    let elements = document.querySelectorAll('.text__neon i');
+    neonLoop(elements)
   }
 }
 </script>
@@ -22,38 +23,22 @@ export default {
   src: url("https://raw.githack.com/hacknug/neon-neue/master/Font%20Files/OpenType%20Webfont/NeonNeue-Regular.otf")
 }
 
-html, body {
-  margin: 0;
-  height: 100%;
-  width: 100%;
-  display: table;
-}
-
-html {
-  background-repeat: repeat;
-  background-position: center center;
-  background-color:#222;
-}
-
 body {
-  background: -moz-radial-gradient(center, ellipse cover, rgba(0,0,0,0.04) 0%, rgba(0,0,0,0.73) 100%);
-  background: -webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(0%,rgba(0,0,0,0.04)), color-stop(100%,rgba(0,0,0,0.73)));
-  background: -webkit-radial-gradient(center, ellipse cover, rgba(0,0,0,0.04) 0%,rgba(0,0,0,0.73) 100%);
-  background: -o-radial-gradient(center, ellipse cover, rgba(0,0,0,0.04) 0%,rgba(0,0,0,0.73) 100%);
-  background: -ms-radial-gradient(center, ellipse cover, rgba(0,0,0,0.04) 0%,rgba(0,0,0,0.73) 100%);
-  background: radial-gradient(ellipse at center, rgba(0,0,0,0.04) 0%,rgba(0,0,0,0.73) 100%);
+  background: #222;
 }
 
 
 .neon_wrapper {
   width: 100%;
   height: 100%;
-  padding: 2rem;
   background: radial-gradient(ellipse at center, rgba(0,0,0,0.04) 0%,rgba(0,0,0,0.73) 100%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  image-rendering: pixelated;
 }
 
 .text__neon {
-  display: table-cell;
   text-align: left;
   vertical-align: middle;
 }
