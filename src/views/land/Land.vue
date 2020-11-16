66d827110aa30919ccb456b9cf5fd4d1ef30d087
git-alice <art.oxbow@gmail.com>
2020-11-16 14:29:55 +0300

    Add main page

--- src/views/land/Land.vue
+++ src/views/land/Land.vue
@@ -1,8 +1,12 @@
 <template lang="pug">
-  .neon_wrapper
-    .text__neon
-      | YO<i>U</i> ARE
-      | <br> WE<i>L</i><i>C</i><i>O</i></span>ME
+  .wrapper
+    .header
+      router-link(to="/home") Home
+    .neon_wrapper
+      .text__neon
+        | YO<i>U</i> ARE
+        | <br> WE<i>L</i><i>C</i><i>O</i></span>ME
+      .overlay
 </template>
 
 <script>
@@ -17,7 +21,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 @font-face {
   font-family: "Neon Neue";
   src: url("https://raw.githack.com/hacknug/neon-neue/master/Font%20Files/OpenType%20Webfont/NeonNeue-Regular.otf")
@@ -27,15 +31,45 @@ body {
   background: #222;
 }
 
+.wrapper {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  .header {
+    width: 100%;
+    height: 200px;
+    padding: 2rem;
+    display: flex;
+    justify-content: space-between;
+  }
+}
 
 .neon_wrapper {
   width: 100%;
   height: 100%;
+  position: relative;
   background: radial-gradient(ellipse at center, rgba(0,0,0,0.04) 0%,rgba(0,0,0,0.73) 100%);
   display: flex;
   justify-content: center;
   align-items: center;
   image-rendering: pixelated;
+
+
+  .overlay {
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    display: block;
+    position: absolute;
+
+    background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAAEAQMAAACTPww9AAAABlBMVEUAAAAAAAClZ7nPAAAAAXRSTlMAQObYZgAAAA1JREFUCNdjOACEQAAACQgBgeEHEZ0AAAAASUVORK5CYII=");
+    background-size: 2px 2px;
+  }
 }
 
 .text__neon {
@@ -45,7 +79,7 @@ body {
 
 i, .text__neon {
   font: normal 100px/ 100px 'Neon Neue', Helvetica, Arial;
-  font-size: 10rem;
+  font-size: 7rem;
   line-height: 1;
   color: rgba(242, 234, 18, 1);
   text-shadow: 0 0 30px rgba(242, 234, 18, 0.5),
