1b3ac646c7b2f4f3285a8947214df56de5b48c62
git-alice <art.oxbow@gmail.com>
2020-11-16 15:25:10 +0300

    Add 404 template

---
+++ src/views/errors/404.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  div
+    img(src="https://alexbsoft.github.io/win95.css/assets/confused_travolta.gif")
+</template>
+
+<script>
+export default {
+name: "404"
+}
+</script>
+
+<style scoped>
+
+</style>
