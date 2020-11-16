0d70dd62616f8711f40484ed8d6395ce537050e9
git-alice <art.oxbow@gmail.com>
2020-11-16 15:09:38 +0300

    Init vue-tour on Home view

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,15 @@
 <template lang="pug">
     div(style="width: 100%; height: 100%", ref="layout")
       // --- All Windows ---
-      div(v-for="(value, name) in getAllWindows()" :key="name")
+      div.w-100.h-100.position-absolute(v-for="(value, name) in getAllWindows()" :key="name")
         keep-alive
           component(:is="name" v-if="value.isOpen" :initOptions="value" :ref="name")
-            div {{ name }}
-            div {{ value }}
+            // div {{ name }}
+            // div {{ value }}
 
       // --- All Icons ---
-      div(v-for="icon in icons")
-        component(:is="icon.component" :window-name="icon.window.name" :src="icon.src" :x="icon.x" :y="icon.y") // :window-name="icon.window.$options.name"
+      div(v-for="(icon, index) in icons")
+        component(:is="icon.component" :data-v-step="index" :window-name="icon.window.name" :src="icon.src" :x="icon.x" :y="icon.y") // :window-name="icon.window.$options.name"
 
       // --- Taskbar ---
       Taskbar
@@ -31,6 +31,31 @@
       // --- TEST BUTTONS ---
       // button(@contextmenu.prevent="$refs.menu.open($event, 'Payload')") Get contextmenu
       // button(@click="testNotification") Get notification
+
+      v-tour(name="myTour" :steps="steps")
+        template(slot-scope='tour')
+          transition(name='fade')
+            v-step.v-customstep(v-for='(step, index) of tour.steps' :key='index' v-if='tour.currentStep === index' :step='step' :previous-step='tour.previousStep' :next-step='tour.nextStep' :stop='tour.stop' :skip='tour.skip' :is-first='tour.isFirst' :is-last='tour.isLast' :labels='tour.labels')
+              div(slot='content')
+                .v-customstep__content
+                  div.v-step__content(v-if='step.content' v-html='step.content')
+                  div(v-else='')
+                    | This is a demo step! The id of this step is {{ hash }} and it targets {{ step.target }}.
+              template(v-if="tour.currentStep === 0")
+                div(slot='actions')
+                  button(@click='tour.finish') Finish
+                  button(@click='tour.nextStep') Next step
+              template(v-else-if="tour.currentStep === tour.steps.length - 1")
+                div(slot='actions')
+                  button(@click='tour.previousStep') Previous step
+                  button(@click='tour.finish') Finish
+              template(v-else)
+                div(slot='actions')
+                  button(@click='tour.finish') Finish
+                  button(@click='tour.previousStep') Previous step
+                  button(@click='tour.nextStep') Next step
+
+
 </template>
 
 <script>
@@ -52,9 +77,35 @@ import icons from "@/business_logic/icons";
 
 export default {
   name: 'Home',
-  data() {
+  data: () => {
     return {
-      icons: icons
+      icons: icons,
+      steps: [
+        {
+          target: '[data-v-step="1"] img',  // We're using document.querySelector() under the hood
+          header: {
+            title: 'Get Started',
+          },
+          content: `Discover <strong>Vue Tour</strong>!`,
+          params: {
+            placement: 'left'
+          }
+        },
+        {
+          target: '[data-v-step="2"] img',
+          content: 'An awesome plugin made with Vue.js!',
+          params: {
+            placement: 'left'
+          }
+        },
+        {
+          target: '[data-v-step="3"] img',
+          content: 'Try it, you\'ll love it!<br>You can put HTML in the steps and completely customize the DOM to suit your needs.',
+          params: {
+            placement: 'top' // Any valid Popper.js placement. See https://popper.js.org/popper-documentation.html#Popper.placements
+          }
+        }
+      ]
     }
   },
   components: {
@@ -111,6 +162,8 @@ export default {
 @import "../../assets/css/tooltip.scss";
 @import "../../assets/css/utils.scss";
 @import "../../assets/css/custom_notification.scss";
+@import "../../assets/css/custom_tour.scss";
+@import "../../assets/css/keyboard_shortcut.scss";
 
 @font-face {
   font-family: "PixelFont";
