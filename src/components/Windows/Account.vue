7516519fc031453fb9e1ffeac5597cca9a35910e
git-alice <art.oxbow@gmail.com>
2020-11-24 00:58:14 +0300

    Try to upload a photo from storage

--- src/components/Windows/Account.vue
+++ src/components/Windows/Account.vue
@@ -2,9 +2,9 @@
   Window(:window-name="this.$options.name" :init-options="initOptions")
     template(v-slot:content)
       img(src="https://www.w3schools.com/images/stickman.gif" width="24" height="39" alt="Stickman")
-      p Full name: {{ user.name }}
-      p Email: {{ user.email }}
-      p Email: {{ user }}
+      p User profile: {{ user.email }}
+      p photoURL:
+        a(:href="photoURL") photo
       button(@click="logout()") Logout
       slot
 </template>
@@ -13,27 +13,38 @@
 import Window from "@/components/Elements/Window/Window";
 import Button from "@/components/Elements/Button/Button";
 
+
 export default {
   name: 'Account',
   data() {
-    return {
-    }
+    return {}
   },
   props: ['initOptions'],
   components: { Button, Window },
   computed: {
     user() {
-      this.$store.dispatch('fetchMe')
-      return this.$store.getters.me;
+      return this.$store.getters.user;
+    },
+    photoURL() {
+      return this.$store.getters.getPhotoURL
     }
   },
   methods: {
-    logout: function () {
-      this.$store.dispatch('logout')
-          .then(() => {
-            this.$router.push('/login')
-          })
-    },
+    // logout: function () {
+    //   this.$store.dispatch('logout')
+    //       .then(() => {
+    //         this.$router.push('/login')
+    //       })
+    // },
+  },
+  mounted() {
+    let user = this.$store.getters.user;
+
+    this.$store.dispatch('getAllPhotos', user)
   }
 }
 </script>
+
+<style lang="scss">
+
+</style>
